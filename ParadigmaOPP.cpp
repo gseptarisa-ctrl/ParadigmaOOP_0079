59bf31ef7a0d86dbf80a6b9787c44842ca1d292c
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-21 14:57:40 +0700

    Menambahkan isi dari representasi class

--- ParadigmaOPP.cpp
+++ ParadigmaOPP.cpp
@@ -12,5 +12,9 @@ class Mahasiswa {
             cout << "Nilai = " << nilai << endl;
         }
 };
-int main(){}
+int main(){
     Mahasiswa mhs1;
+
+    mhs1.nim = 111;
+    mhs1.nama = "Joko Susilo";
+   
